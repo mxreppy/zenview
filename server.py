86ab9286603d5238867253104356128d1e1e7ed2
Ethan McCreadie <ethanmcc@gmail.com>
2014-09-19 12:18:38 -0400

    Rename app to application

--- server.py
+++ server.py
@@ -3,8 +3,8 @@ from flask import Flask, jsonify
 
 from zd_lib import get_zd_url
 
-app = Flask(__name__, static_folder="dist")
-app.debug = True
+application = Flask(__name__, static_folder="dist")
+application.debug = True
 
 
 @app.route("/api/zendesk/ticket/")
@@ -14,19 +14,19 @@ def get_zd_tickets():
 
 @app.route('/')
 def root():
-    return app.send_static_file('index.html')
+    return application.send_static_file('index.html')
 
 
 @app.route('/js/<path:path>')
 def static_proxy(path):
     # send_static_file will guess the correct MIME type
-    return app.send_static_file(os.path.join('js', path))
+    return application.send_static_file(os.path.join('js', path))
 
 
 @app.route('/views/<path:path>')
 def static_proxy_views(path):
-    return app.send_static_file(os.path.join('views', path))
+    return application.send_static_file(os.path.join('views', path))
 
 
 if __name__ == "__main__":
-    app.run()
+    application.run()
